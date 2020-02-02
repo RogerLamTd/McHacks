b8fb0ba66805b7184cb5c43c3aa731d2e7663515
BryanHeYang <59769290+BryanHeYang@users.noreply.github.com>
2020-02-02 02:52:37 -0500

    Update App.js

--- mclet/src/App.js
+++ mclet/src/App.js
@@ -6,16 +6,6 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 require('dotenv').config();
 
 // ...
-
-
-
-
-const testObject = {
-  lat: "45.5",
-  lng:"-73.57"
-}
-
-const testJson = JSON.stringify(testObject);
 let id = 0;
 const listings = [];
 
@@ -49,58 +39,12 @@ const getData = async () => {
 export class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {lat : 0,
-    lng : 0};
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     getData();
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-
-  }
-
-  handleClick(){
-    let newObject = {
-      lat : this.state.lat,
-      lng : this.state.lng
-    }
-
-    addMarker(JSON.stringify(newObject));
-    this.forceUpdate();
   }
 
   render(){
     return (  
-      
       <div className="App">
-        <form>
-          <label>
-              Enter lat:
-              <input
-                name="lat"
-                type="text"
-                value={this.state.lat}
-                onChange={this.handleInputChange} />
-            </label>
-            <br />
-            <label>
-              Enter lng:
-              <input
-                name="lng"
-                type="text"
-                value={this.state.lng}
-                onChange={this.handleInputChange} />
-            </label>
-        </form>
-        <button onClick = {this.handleClick}>submit</button>
-         
         <SubletMap
           isMarkerShown
           googleMapURL={"https://maps.googleapis.com/maps/api/js?key="+process.env.REACT_APP_MAP_KEY}
@@ -113,8 +57,6 @@ export class App extends React.Component{
     );
   }
 }
-
-
 
 export default App;
 
